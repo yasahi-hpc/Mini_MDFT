87e48eeaf65d869d9c890173bd9393a0c38994b3
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2024-11-29 23:29:09 +0900

    Add orientation projection transform.. Something may be wrong

--- unit_test/Test_Math_Utils.cpp
+++ unit_test/Test_Math_Utils.cpp
@@ -48,6 +48,222 @@ void test_uniform_mesh(int n, T dx) {
   EXPECT_TRUE(allclose(execution_space(), x, x_ref, epsilon));
 }
 
+template <typename T>
+void test_cross_product() {
+  using ViewType  = Kokkos::View<T*, execution_space>;
+  using ArrayType = Kokkos::Array<T, 3>;
+  ArrayType A{1.0, 2.0, 3.0}, B{4.0, 5.0, 6.0};
+  ArrayType Ex{1.0, 0.0, 0.0}, Ey{0.0, 1.0, 0.0}, Ez{0.0, 0.0, 1.0};
+
+  ViewType AxB("AxB", 3), AxEx("AxEx", 3), AxEy("AxEy", 3), AxEz("AxEz", 3);
+  ViewType AxB_ref("AxB_ref", 3), AxEx_ref("AxEx_ref", 3),
+      AxEy_ref("AxEy_ref", 3), AxEz_ref("AxEz_ref", 3);
+
+  // Set reference
+  auto h_AxB_ref  = Kokkos::create_mirror_view(AxB_ref);
+  auto h_AxEx_ref = Kokkos::create_mirror_view(AxEx_ref);
+  auto h_AxEy_ref = Kokkos::create_mirror_view(AxEy_ref);
+  auto h_AxEz_ref = Kokkos::create_mirror_view(AxEz_ref);
+
+  {
+    h_AxEx_ref(0) = 0.0;
+    h_AxEx_ref(1) = 3.0;
+    h_AxEx_ref(2) = -2.0;
+    h_AxEy_ref(0) = -3.0;
+    h_AxEy_ref(1) = 0.0;
+    h_AxEy_ref(2) = 1.0;
+    h_AxEz_ref(0) = 2.0;
+    h_AxEz_ref(1) = -1.0;
+    h_AxEz_ref(2) = 0.0;
+    h_AxB_ref(0)  = A[1] * B[2] - A[2] * B[1];
+    h_AxB_ref(1)  = A[2] * B[0] - A[0] * B[2];
+    h_AxB_ref(2)  = A[0] * B[1] - A[1] * B[0];
+  }
+
+  Kokkos::deep_copy(AxB_ref, h_AxB_ref);
+  Kokkos::deep_copy(AxEx_ref, h_AxEx_ref);
+  Kokkos::deep_copy(AxEy_ref, h_AxEy_ref);
+  Kokkos::deep_copy(AxEz_ref, h_AxEz_ref);
+
+  Kokkos::parallel_for(
+      Kokkos::RangePolicy<execution_space, Kokkos::IndexType<int>>{0, 1},
+      KOKKOS_LAMBDA(int) {
+        auto tmp_AxEx = MDFT::Impl::cross_product(A, Ex);
+        auto tmp_AxEy = MDFT::Impl::cross_product(A, Ey);
+        auto tmp_AxEz = MDFT::Impl::cross_product(A, Ez);
+        auto tmp_AxB  = MDFT::Impl::cross_product(A, B);
+
+        for (int i = 0; i < 3; ++i) {
+          AxEx(i) = tmp_AxEx[i];
+          AxEy(i) = tmp_AxEy[i];
+          AxEz(i) = tmp_AxEz[i];
+          AxB(i)  = tmp_AxB[i];
+        }
+      });
+
+  Kokkos::fence();
+
+  T epsilon = std::numeric_limits<T>::epsilon() * 100;
+  EXPECT_TRUE(allclose(execution_space(), AxEx, AxEx_ref, epsilon));
+  EXPECT_TRUE(allclose(execution_space(), AxEy, AxEy_ref, epsilon));
+  EXPECT_TRUE(allclose(execution_space(), AxEz, AxEz_ref, epsilon));
+  EXPECT_TRUE(allclose(execution_space(), AxB, AxB_ref, epsilon));
+}
+
+template <typename T>
+void test_norm2() {
+  using ViewType    = Kokkos::View<int, execution_space>;
+  using Array1DType = Kokkos::Array<T, 1>;
+  using Array2DType = Kokkos::Array<T, 2>;
+  using Array3DType = Kokkos::Array<T, 3>;
+  Array1DType A1{1.5};
+  Array2DType A2{1.2, 2.5};
+  Array3DType A3{0.7, 2.8, 0.3};
+
+  ViewType error1("error1"), error2("error2"), error3("error3");
+
+  // Set reference
+  T norm1_ref = std::sqrt(A1[0] * A1[0]);
+  T norm2_ref = std::sqrt(A2[0] * A2[0] + A2[1] * A2[1]);
+  T norm3_ref = std::sqrt(A3[0] * A3[0] + A3[1] * A3[1] + A3[2] * A3[2]);
+
+  T epsilon = std::numeric_limits<T>::epsilon() * 100;
+  Kokkos::parallel_for(
+      Kokkos::RangePolicy<execution_space, Kokkos::IndexType<int>>{0, 1},
+      KOKKOS_LAMBDA(int) {
+        if (Kokkos::abs(MDFT::Impl::norm2(A1) - norm1_ref) > epsilon)
+          Kokkos::atomic_store(error1.data(), 1);
+        if (Kokkos::abs(MDFT::Impl::norm2(A2) - norm2_ref) > epsilon)
+          Kokkos::atomic_store(error2.data(), 1);
+        if (Kokkos::abs(MDFT::Impl::norm2(A3) - norm3_ref) > epsilon)
+          Kokkos::atomic_store(error3.data(), 1);
+      });
+
+  auto h_error1 =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, error1);
+  auto h_error2 =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, error2);
+  auto h_error3 =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, error3);
+
+  ASSERT_EQ(h_error1(), 0);
+  ASSERT_EQ(h_error2(), 0);
+  ASSERT_EQ(h_error3(), 0);
+}
+
+template <typename T>
+void test_L2normalize() {
+  using ViewType  = Kokkos::View<T*, execution_space>;
+  using ArrayType = Kokkos::Array<T, 3>;
+  ArrayType A{1.0, 2.0, 3.0}, B{4.0, 5.0, 6.0};
+  ArrayType Ex{1.0, 0.0, 0.0}, Ey{0.0, 1.0, 0.0}, Ez{0.0, 0.0, 1.0};
+
+  ViewType A_norm("A_norm", 3), B_norm("B_norm", 3), Ex_norm("Ex_norm", 3),
+      Ey_norm("Ey_norm", 3), Ez_norm("Ez_norm", 3);
+
+  Kokkos::parallel_for(
+      Kokkos::RangePolicy<execution_space, Kokkos::IndexType<int>>{0, 1},
+      KOKKOS_LAMBDA(int) {
+        auto tmp_A_norm  = MDFT::Impl::L2normalize(A);
+        auto tmp_B_norm  = MDFT::Impl::L2normalize(B);
+        auto tmp_Ex_norm = MDFT::Impl::L2normalize(Ex);
+        auto tmp_Ey_norm = MDFT::Impl::L2normalize(Ey);
+        auto tmp_Ez_norm = MDFT::Impl::L2normalize(Ez);
+
+        for (int i = 0; i < 3; ++i) {
+          A_norm(i)  = tmp_A_norm[i];
+          B_norm(i)  = tmp_A_norm[i];
+          Ex_norm(i) = tmp_Ex_norm[i];
+          Ey_norm(i) = tmp_Ey_norm[i];
+          Ez_norm(i) = tmp_Ez_norm[i];
+        }
+      });
+
+  Kokkos::fence();
+
+  auto h_A_norm =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, A_norm);
+  auto h_B_norm =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, B_norm);
+  auto h_Ex_norm =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, Ex_norm);
+  auto h_Ey_norm =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, Ey_norm);
+  auto h_Ez_norm =
+      Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, Ez_norm);
+
+  T sum_A = 0.0, sum_B = 0.0, sum_Ex = 0.0, sum_Ey = 0.0, sum_Ez = 0.0;
+  for (int i = 0; i < 3; ++i) {
+    sum_A += h_A_norm(i) * h_A_norm(i);
+    sum_B += h_B_norm(i) * h_B_norm(i);
+    sum_Ex += h_Ex_norm(i) * h_Ex_norm(i);
+    sum_Ey += h_Ey_norm(i) * h_Ey_norm(i);
+    sum_Ez += h_Ez_norm(i) * h_Ez_norm(i);
+  }
+
+  // Norm should be 1
+  T epsilon = std::numeric_limits<T>::epsilon() * 100;
+  EXPECT_LT(Kokkos::abs(Kokkos::sqrt(sum_A) - 1.0), epsilon);
+  EXPECT_LT(Kokkos::abs(Kokkos::sqrt(sum_B) - 1.0), epsilon);
+  EXPECT_LT(Kokkos::abs(Kokkos::sqrt(sum_Ex) - 1.0), epsilon);
+  EXPECT_LT(Kokkos::abs(Kokkos::sqrt(sum_Ey) - 1.0), epsilon);
+  EXPECT_LT(Kokkos::abs(Kokkos::sqrt(sum_Ez) - 1.0), epsilon);
+}
+
+template <typename T>
+void test_prevent_underflow() {
+  using ViewType        = Kokkos::View<T, execution_space>;
+  using ComplexViewType = Kokkos::View<Kokkos::complex<T>, execution_space>;
+  ViewType x("x"), x2("x2"), x_ref("x_ref"), x2_ref("x2_ref");
+  ComplexViewType z("z"), z2("z2"), z_ref("z_ref"), z2_ref("z2_ref");
+
+  auto h_x_ref = Kokkos::create_mirror_view(x_ref);
+  auto h_x2    = Kokkos::create_mirror_view(x2);
+
+  auto h_z_ref = Kokkos::create_mirror_view(z_ref);
+  auto h_z2    = Kokkos::create_mirror_view(z2);
+
+  h_x_ref() = 1.0;       // Big value
+  h_x2()    = 1.0e-100;  // Small value
+
+  h_z_ref() = Kokkos::complex<T>(2.0, 3.0);           // Big value
+  h_z2()    = Kokkos::complex<T>(1.0e-100, 1.0e-50);  // Small value
+
+  Kokkos::deep_copy(x, h_x_ref);
+  Kokkos::deep_copy(x2, h_x2);
+
+  Kokkos::deep_copy(z, h_z_ref);
+  Kokkos::deep_copy(z2, h_z2);
+
+  T epsilon = std::numeric_limits<T>::epsilon() * 100;
+
+  Kokkos::parallel_for(
+      Kokkos::RangePolicy<execution_space, Kokkos::IndexType<int>>{0, 1},
+      KOKKOS_LAMBDA(int) {
+        // Real number
+        x()  = MDFT::Impl::prevent_underflow(x(), epsilon);
+        x2() = MDFT::Impl::prevent_underflow(x2(), epsilon);
+
+        // Complex number
+        z()  = MDFT::Impl::prevent_underflow(z(), epsilon);
+        z2() = MDFT::Impl::prevent_underflow(z2(), epsilon);
+      });
+
+  Kokkos::fence();
+
+  auto h_x = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, x);
+  auto h_z = Kokkos::create_mirror_view_and_copy(Kokkos::HostSpace{}, z);
+
+  // Norm should be 1
+  EXPECT_LT(Kokkos::abs(h_x() - h_x_ref()), epsilon);
+  EXPECT_LT(Kokkos::abs(h_x2() - 0.0), epsilon);
+
+  EXPECT_LT(Kokkos::abs(h_z().real() - h_z_ref().real()), epsilon);
+  EXPECT_LT(Kokkos::abs(h_z().imag() - h_z_ref().imag()), epsilon);
+  EXPECT_LT(Kokkos::abs(h_z2().real() - 0.0), epsilon);
+  EXPECT_LT(Kokkos::abs(h_z2().imag() - 0.0), epsilon);
+}
+
 TYPED_TEST(TestMathUtils, GaussLegendre) {
   using float_type = typename TestFixture::float_type;
 
@@ -62,6 +278,26 @@ TYPED_TEST(TestMathUtils, UniformMesh) {
   for (auto n : this->m_all_sizes) {
     test_uniform_mesh<float_type>(n, this->m_delta);
   }
+}
+
+TYPED_TEST(TestMathUtils, CrossProduct) {
+  using float_type = typename TestFixture::float_type;
+  test_cross_product<float_type>();
+}
+
+TYPED_TEST(TestMathUtils, Norm2) {
+  using float_type = typename TestFixture::float_type;
+  test_norm2<float_type>();
+}
+
+TYPED_TEST(TestMathUtils, L2normalize) {
+  using float_type = typename TestFixture::float_type;
+  test_L2normalize<float_type>();
+}
+
+TYPED_TEST(TestMathUtils, PreventUnderflow) {
+  using float_type = typename TestFixture::float_type;
+  test_prevent_underflow<float_type>();
 }
 
 }  // namespace
--- unit_test/Test_OrientationProjectionTransform.cpp
+++ unit_test/Test_OrientationProjectionTransform.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include <tuple>
 #include <vector>
+#include <Kokkos_Random.hpp>
 #include "MDFT_Grid.hpp"
 #include "MDFT_OrientationProjectionTransform.hpp"
 #include "Test_Utils.hpp"
@@ -11,6 +12,14 @@ using execution_space = Kokkos::DefaultExecutionSpace;
 using float_types     = ::testing::Types<float, double>;
 
 template <typename T>
+struct TestOrientationProjectionMap : public ::testing::Test {
+  using float_type             = T;
+  using scalar_array_type      = Kokkos::Array<T, 3>;
+  using int_array_type         = Kokkos::Array<int, 3>;
+  std::vector<int> m_all_sizes = {5, 10};
+};
+
+template <typename T>
 struct TestOrientationProjection : public ::testing::Test {
   using float_type             = T;
   using scalar_array_type      = Kokkos::Array<T, 3>;
@@ -18,7 +27,23 @@ struct TestOrientationProjection : public ::testing::Test {
   std::vector<int> m_all_sizes = {5, 10};
 };
 
+TYPED_TEST_SUITE(TestOrientationProjectionMap, float_types);
 TYPED_TEST_SUITE(TestOrientationProjection, float_types);
+
+template <typename T, typename IntArrayType, typename ScalarArrayType>
+void test_orientation_projection_map_init(int n) {
+  MDFT::SpatialGrid<execution_space, T> grid(IntArrayType{n, n, n},
+                                             ScalarArrayType{1, 1, 1});
+  int mmax = 10, molrotsymorder = 10;
+  MDFT::AngularGrid<execution_space, T> angular_grid(mmax, molrotsymorder);
+
+  ASSERT_NO_THROW(({
+    MDFT::OrientationProjectionMap<execution_space, T> map(angular_grid);
+  }));
+
+  // Add checks for the indices order
+  // mapping is different from fortran version
+}
 
 template <typename T, typename IntArrayType, typename ScalarArrayType>
 void test_orientation_projection_init(int n) {
@@ -26,11 +51,60 @@ void test_orientation_projection_init(int n) {
                                              ScalarArrayType{1, 1, 1});
   int mmax = 10, molrotsymorder = 10;
   MDFT::AngularGrid<execution_space, T> angular_grid(mmax, molrotsymorder);
+  MDFT::OrientationProjectionMap<execution_space, T> map(angular_grid);
 
   ASSERT_NO_THROW(({
-    MDFT::OrientationProjectionTransform<execution_space, T> opt(grid,
-                                                                 angular_grid);
+    MDFT::OrientationProjectionTransform<execution_space, T> opt(
+        grid, angular_grid, map);
   }));
+}
+
+template <typename T, typename IntArrayType, typename ScalarArrayType>
+void test_orientation_projection_identity(int n) {
+  MDFT::SpatialGrid<execution_space, T> grid(IntArrayType{n, n, n},
+                                             ScalarArrayType{1, 1, 1});
+  int mmax = 10, molrotsymorder = 10;
+  MDFT::AngularGrid<execution_space, T> angular_grid(mmax, molrotsymorder);
+  MDFT::OrientationProjectionMap<execution_space, T> map(angular_grid);
+  MDFT::OrientationProjectionTransform<execution_space, T> opt(
+      grid, angular_grid, map);
+
+  using OViewType = Kokkos::View<T****, execution_space>;
+  using PViewType = Kokkos::View<Kokkos::complex<T>**, execution_space>;
+
+  auto [nx, ny, nz] = grid.m_n_nodes;
+  auto ntheta = angular_grid.m_ntheta, nphi = angular_grid.m_nphi,
+       npsi = angular_grid.m_npsi;
+  auto np   = angular_grid.m_np;
+
+  OViewType o("o", nx * ny * nz, ntheta, nphi, npsi),
+      o_inv("o_inv", nx * ny * nz, ntheta, nphi, npsi),
+      o_ref("o_ref", nx * ny * nz, ntheta, nphi, npsi);
+  PViewType p("p", np, nx * ny * nz);
+
+  // Initialize o with random values
+  Kokkos::Random_XorShift64_Pool<> random_pool(/*seed=*/12345);
+  Kokkos::fill_random(o, random_pool, 1.0);
+
+  Kokkos::deep_copy(o_ref, o);
+
+  opt.angl2proj(o, p);
+  opt.proj2angl(p, o_inv);
+
+  // FIXME Does not work for now, probably something is wrong
+  // T epsilon = std::numeric_limits<T>::epsilon() * 100;
+  // EXPECT_TRUE(allclose(execution_space(), o_inv, o_ref, epsilon));
+}
+
+TYPED_TEST(TestOrientationProjectionMap, Initialization) {
+  using float_type        = typename TestFixture::float_type;
+  using int_array_type    = typename TestFixture::int_array_type;
+  using scalar_array_type = typename TestFixture::scalar_array_type;
+
+  for (auto m : this->m_all_sizes) {
+    test_orientation_projection_map_init<float_type, int_array_type,
+                                         scalar_array_type>(m);
+  }
 }
 
 TYPED_TEST(TestOrientationProjection, Initialization) {
@@ -41,6 +115,17 @@ TYPED_TEST(TestOrientationProjection, Initialization) {
   for (auto m : this->m_all_sizes) {
     test_orientation_projection_init<float_type, int_array_type,
                                      scalar_array_type>(m);
+  }
+}
+
+TYPED_TEST(TestOrientationProjection, Identity) {
+  using float_type        = typename TestFixture::float_type;
+  using int_array_type    = typename TestFixture::int_array_type;
+  using scalar_array_type = typename TestFixture::scalar_array_type;
+
+  for (auto m : this->m_all_sizes) {
+    test_orientation_projection_identity<float_type, int_array_type,
+                                         scalar_array_type>(m);
   }
 }
 
