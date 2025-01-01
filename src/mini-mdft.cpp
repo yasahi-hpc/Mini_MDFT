736fbac64937ad47387c99207e32e07355f6965f
Yuuichi Asahi <y.asahi@nr.titech.ac.jp>
2025-01-01 10:31:06 +0900

    Apply format under src

--- src/mini-mdft.cpp
+++ src/mini-mdft.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 using execution_space = Kokkos::DefaultExecutionSpace;
-using float_type = double;
+using float_type      = double;
 
 int main(int argc, char *argv[]) {
   Kokkos::initialize(argc, argv);
@@ -12,14 +12,14 @@ int main(int argc, char *argv[]) {
     MDFT::Solver<execution_space, float_type> solver(exec);
 
     try {
-        solver.initialize(&argc, &argv);
-        solver.run();
-        solver.finalize();
+      solver.initialize(&argc, &argv);
+      solver.run();
+      solver.finalize();
 
-        return 0;
-    } catch(std::runtime_error e) {
-        std::cerr << e.what() << std::endl;
-        solver.finalize();
+      return 0;
+    } catch (std::runtime_error e) {
+      std::cerr << e.what() << std::endl;
+      solver.finalize();
     }
   }
   Kokkos::finalize();
