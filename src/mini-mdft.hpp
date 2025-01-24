fd2f33b500cfaa302b0e1f4c5897688a0acf9738
yasahi-hpc <57478230+yasahi-hpc@users.noreply.github.com>
2025-01-24 10:14:40 +0100

    allocate vexc view (#50)
    * allocate vexc view
    
    * reshaping vexc
    
    * fix: format
    
    ---------
    
    Co-authored-by: ua02541 <y.asahi@nr.titech.ac.jp>

--- src/mini-mdft.hpp
+++ src/mini-mdft.hpp
@@ -98,6 +98,7 @@ class Solver {
     auto np     = m_angular_grid->m_np;
 
     m_delta_rho   = View4DType("delta_rho", nx * ny * nz, ntheta, nphi, npsi);
+    m_vexc        = View4DType("vexc", nx * ny * nz, ntheta, nphi, npsi);
     m_delta_rho_p = ComplexView4DType("delta_rho_p", np, nx, ny, nz);
 
     // Initialize solute
@@ -151,12 +152,17 @@ class Solver {
     // (nm * nmup * nmu, nx, ny, nz) -> (nm * nmup * nmu, nx, ny, nz)
     m_conv->execute(m_delta_rho_p);
 
-    // 8. gather projections
+    // 8. to orientation
     // (np, nx, ny, nz) -> (nx * ny * nz, theta, phi, psi)
     m_op_transform->proj2angl(m_delta_rho_p, m_vexc);
 
-    // (nx, ny, nz, theta * phi * psi) -> (nx * ny * nz, theta, phi, psi)
-    get_delta_f(m_exec_space, solvent.m_xi, m_vexc, m_angular_grid->m_w, df, ff,
+    // 9. gather projections
+    // (nx, ny, nz, theta * phi * psi) -> (nx, ny, nz, theta * phi * psi)
+    auto xi = solvent.m_xi;
+    int nx = xi.extent(0), ny = xi.extent(1), nz = xi.extent(2),
+        no = xi.extent(3);
+    View4DType vexc(m_vexc.data(), nx, ny, nz, no);
+    get_delta_f(m_exec_space, solvent.m_xi, vexc, m_angular_grid->m_w, df, ff,
                 solvent.m_rho0, m_solvents->m_prefactor);
   }
 };  // class Solver
